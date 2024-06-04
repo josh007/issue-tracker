dfbf276675440cc71d1beb0b998ce3622a378360
Josh007 <eyassu@live.com>
2024-06-04 15:34:46 +0300

    Added a delete button

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@radix-ui/themes";
+
+export const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return <Button color="red">Delete Issue</Button>;
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import "./globals.css";
 import NavBar from "./NavBar";
 
@@ -23,7 +23,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme accentColor="blue" grayColor="sand" radius="large">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
           {/* <ThemePanel /> */}
         </Theme>
       </body>
