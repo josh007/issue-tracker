f396051159a1a5ffdedcbc50107771f3ed14afb5
Josh007 <eyassu@live.com>
2024-06-04 15:58:16 +0300

    Show a confimtation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,37 @@
-import { Button } from "@radix-ui/themes";
+import { MinusCircledIcon, Pencil2Icon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
-export const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color="red">Delete Issue</Button>;
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <MinusCircledIcon />
+          Delete Issue
+          {/* <Link href={`/issues/${issueId}/edit`}></Link> */}
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This issue will no longer
+          be accessible.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
+
+export default DeleteIssueButton;
