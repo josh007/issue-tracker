dd95eac35571b36dfb2977d4f5c180b53a4fc758
Josh007 <eyassu@live.com>
2024-06-04 17:41:27 +0300

    secured the app

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,51 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 
 const handler = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Username", type: "email", placeholder: "email...." },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        if (!credentials?.email || !credentials.password) return null;
+        console.log(credentials);
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        console.log(user);
+
+        if (!user) return null;
+        console.log(user);
+
+        // const matched = await bcrypt.compare(
+        //   credentials.password,
+        //   user.hashedPassword!
+        // );
+        const matched = true;
+
+        return matched ? user : null;
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
 });
 
 export { handler as GET, handler as POST };
