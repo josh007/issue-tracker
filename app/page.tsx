848ff674297600819dfed4b4ffe260beda04a63d
Josh007 <eyassu@live.com>
2024-06-06 16:46:03 +0300

    build the issue summary card for the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import Pagination from "./components/Pagination";
-import LatestIssues from "./components/LatestIssues";
+import prisma from "@/prisma/client";
+import IssueSummary from "./components/IssueSummary";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
+  const inProgresss = await prisma.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
+  const closed = await prisma.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
+
+  return <IssueSummary open={open} inProgress={inProgresss} closed={closed} />;
 }
